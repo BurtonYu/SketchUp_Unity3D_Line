35e95c6a57dcd3f0d2a8ea6cd7ec7a69708cd953
Burtonyu <18717831586@163.com>
2016-06-28 09:48:34 +0800

    release0,0

---
+++ ReadXML/Assets/test/Demo.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+using Assets;
+
+public class Demo : MonoBehaviour
+{
+    private LineRenderer line;
+    public Color c = Color.yellow;
+    public float wid = 0.05F;
+    private Vector3[] vec1;
+    private Vector3[] vec2;
+    private Vector3[] points; 
+    private ReadXml r=new ReadXml();
+    int i;
+    public GameObject prefab;
+    private GameObject copy;
+
+  
+    //public int lengthOfLineRenderer = 20;
+    void Start()
+    {
+        points = new Vector3[2];
+        r.gitv();
+        vec1 = r.v1;
+        vec2 = r.v2;
+
+        for (i = 0; i < vec1.Length; i++)
+        {
+            points[0] = vec1[i];
+            points[1] = vec2[i];
+            //copy=Instantiate(prefab);
+            copy = Instantiate(prefab, transform.position, transform.rotation) as GameObject;
+            copy.transform.parent = transform;
+            line = copy.GetComponent<LineRenderer>();
+            //line.material = new Material(Shader.Find("Particles/Multiply"));
+            //line.material = new Material(Shader.Find("Custom/test"));
+            line.material = new Material(Shader.Find("Custom/NewSurfaceShader"));
+            line.useLightProbes = false;
+            line.receiveShadows = false;
+            line.shadowCastingMode = ShadowCastingMode.Off;
+            line.SetColors(c, c);
+            line.SetWidth(wid, wid);
+
+            line.SetPositions(points);
+
+        }
+    }
+
+    void Update() {   
+    }
+
+}
---
+++ ReadXML/Assets/test/ReadXml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using UnityEngine;
+using System.Collections;
+
+namespace Assets
+{
+    class ReadXml
+    {
+        XmlDocument xmlDoc;
+        public int i=0;
+        public Vector3[] v1;
+        public Vector3[] v2;
+        float x = 0, y = 0, z = 0;
+
+        //显示xml数据
+        public void gitv()
+        {
+            xmlDoc = new XmlDocument();
+            xmlDoc.Load("shu_bs_q.edges.xml"); //加载xml文件
+            XmlNode xn = xmlDoc.SelectSingleNode("edges");
+            XmlNodeList xnl = xn.ChildNodes;
+            v1 = new Vector3[xnl.Count];
+            v2= new Vector3[xnl.Count];
+            foreach (XmlNode xnf in xnl)
+            {
+                XmlNodeList xnl2 = xnf.ChildNodes;
+                foreach (XmlNode xnf2 in xnl2)
+                {
+                    XmlElement xe = (XmlElement)xnf2;
+                    x = float.Parse(xe.GetAttribute("x"));
+                    y = float.Parse(xe.GetAttribute("y"));
+                    z = -float.Parse(xe.GetAttribute("z"));
+                    if (xe.Name == "start")
+                    {
+                        v1[i].Set(x, y, z);
+                    }
+                    else {
+                        v2[i].Set(x, y, z);
+                    }
+
+                }             
+                i++;
+            }
+        }
+
+
+    }
+}
